cc1e098bb3658fc07fb50d7e01c74e8ef83fe3b7
qxm <1135422394@qq.com>
2020-12-30 17:45:35 +0800

    进行输出打印数据3

--- src/main/java/com/demo/entity/aaa.java
+++ src/main/java/com/demo/entity/aaa.java
@@ -12,5 +12,9 @@ public class aaa {
         System.out.println(11);
         System.out.println(12);
         System.out.println(13);
+        System.out.println(1);
+        System.out.println(11);
+        System.out.println(12);
+        System.out.println(13);
     }
 }
